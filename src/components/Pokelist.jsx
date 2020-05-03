9dd8fdbb767fa96cd549d833a7a963b1a29e8bc9
illninofan <ukayani202@gmail.com>
2020-05-04 00:17:13 +0100

    base code for UI styling

---
+++ src/components/Pokelist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/PokeList.css';
+
+const Pokelist = (props) => {
+    return (
+    <section className='poke-list'>
+
+    </section>
+    )
+}
+
+export default Pokelist
